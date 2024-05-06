e948a285f3e56b8b78e499af4a8d5515d9019fe0
Binh Pham <binh.pham@synaigy.com>
2024-05-06 11:16:29 +0200

    init commit

---
+++ src/test/java/com/syn/demo/TcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
